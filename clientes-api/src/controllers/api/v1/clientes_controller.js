7b0b91f62e961d8aa2fbef68bf0551d398efc7c9
Jovanio-Junior <jovaniojr3@gmail.com>
2022-01-28 10:48:07 -0300

    update

--- clientes-api/src/controllers/api/v1/clientes_controller.js
+++ clientes-api/src/controllers/api/v1/clientes_controller.js
@@ -1,18 +1,30 @@
 //Clientes Controller
 let validarCpf = require('./validar_cpf')
-
+let formatarCpf = require('./formatar_cpf')
+const formatar_cpf = require('./formatar_cpf')
 class ClientesController {
     async get(req, res) {
         let body = req.body
         if (body.nome && body.cpf && body.estado) {
             let cpf = body.cpf
-            if (validarCpf(cpf)) {
-                cpf =
+
+            if(body.cpf.length >= 11 || body.cpf.length <=14)
+            {
+                cpf = formatar_cpf.formataCpfCadastro(cpf)
+                if (validarCpf(cpf)) {
+
+                } else {
+                    res.statusCode = 406
+                    res.send({
+                        err: "CPF invalido."
+                    })
+                }
             } else {
                 res.statusCode = 406
                 res.send({
                     err: "CPF invalido."
                 })
+
             }
         } else {
             res.statusCode = 400
---
+++ clientes-api/src/controllers/api/v1/formatar_cpf.js
@@ -0,0 +1,17 @@
+function formataCpfCadastro(cpf){
+    //retira os caracteres indesejados...
+    cpf = cpf.replace(/[^\d]/g, "");
+    
+    //realizar a formatação...
+      return cpf.replace(/(\d{2})(\d{2})(\d{2})(\d{1})/, "$1$2$3$4");
+  }
+
+  function formataCpfConsulta(cpf){
+    //retira os caracteres indesejados...
+    cpf = cpf.replace(/[^\d]/g, "");
+    
+    //realizar a formatação...
+      return cpf.replace(/(\d{3})(\d{3})(\d{3})(\d{2})/, "$1.$2.$3-$4");
+  }
+  
+  module.exports = [formataCpfCadastro, formataCpfConsulta]
