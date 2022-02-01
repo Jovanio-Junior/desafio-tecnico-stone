bd0293f3510bdb559cb5c1e9e3dea0ccb3878387
Jovanio-Junior <jovaniojr3@gmail.com>
2022-02-01 06:36:59 -0300

    update

--- database/dbNoSQL.js
+++
@@ -1,7 +0,0 @@
-const firebaseConfig = {
-    apiKey: 'AIzaSyA2qSdn7790DcvpXUvQWo7m0UUrRzEZ5ZE',
-    authDomain: 'stone-b58f2.firebaseapp.com',
-    projectId: 'stone-b58f2'
-};
-
-module.exports = firebaseConfig
